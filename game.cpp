5f587b9665272f80da10d3fb96ad1359b73f334f
sezer esim <sezeresim@gmail.com>
2019-12-24 02:50:37 +0100

    menubutton

--- game.cpp
+++ game.cpp
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <iostream>
 #include <Windows.h>
-
 #include <gl\GL.h>
 #include <gl\GLU.h>
 #include <GL/glut.h>
 #include <math.h>
-#include <cstring>
+
+#include <string>
 
 #define M_PI 3.14159265358979323846
 
@@ -24,9 +24,12 @@ bool windowStart=false;
 float windowY=-21;
 float windowSpeed=0.05;
 float timeWindow=0;
-//
-bool startGame=false;
 
+//chosescreen
+int  activeScreen=1;
+//menu
+int activeButton=3;
+//initials
 void init()
 {
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -36,27 +39,25 @@ void init()
     glMatrixMode(GL_MODELVIEW);
     glEnable(GL_DEPTH_TEST);
 }
-/*
- Drawing text 2D screen.
-*/
+ //Drawing text 2D screen.
 void drawText(const char *text, int length, int x, int y){
- glMatrixMode(GL_PROJECTION); // change the current matrix to PROJECTION
- double matrix[16]; // allocate 16-byte in memory (thanks to BlueByteGames about memory-leak warning comment on YouTube)
- glGetDoublev(GL_PROJECTION_MATRIX, matrix); // get the values from PROJECTION matrix to local variable
- glLoadIdentity(); // reset PROJECTION matrix to identity matrix
- glOrtho(0, 800, 0, 600, -5, 5); // orthographic perspective
- glMatrixMode(GL_MODELVIEW); // change current matrix to MODELVIDE matrix again
- glLoadIdentity(); // reset it to identity matrix
- glPushMatrix(); // push current state of MODELVIEW matrix to stack
- glLoadIdentity(); // reset it again. (may not be required, but it my convention
- glRasterPos2i(x, y); // raster position in 2D
- for(int i=0; i<length; i++){
-  glutBitmapCharacter(GLUT_BITMAP_9_BY_15, (int)text[i]); // generation of characters in our text with 9 by 15 GLU font
- }
- glPopMatrix(); // get MODELVIEW matrix value from stack
- glMatrixMode(GL_PROJECTION); // change current matrix mode to PROJECTION
- glLoadMatrixd(matrix); // reset
- glMatrixMode(GL_MODELVIEW); // change current matrix mode to MODELVIEW
+     glMatrixMode(GL_PROJECTION); // change the current matrix to PROJECTION
+     double matrix[16]; // allocate 16-byte in memory (thanks to BlueByteGames about memory-leak warning comment on YouTube)
+     glGetDoublev(GL_PROJECTION_MATRIX, matrix); // get the values from PROJECTION matrix to local variable
+     glLoadIdentity(); // reset PROJECTION matrix to identity matrix
+     glOrtho(0, 800, 0, 600, -5, 5); // orthographic perspective
+     glMatrixMode(GL_MODELVIEW); // change current matrix to MODELVIDE matrix again
+     glLoadIdentity(); // reset it to identity matrix
+     glPushMatrix(); // push current state of MODELVIEW matrix to stack
+     glLoadIdentity(); // reset it again. (may not be required, but it my convention
+     glRasterPos2i(x, y); // raster position in 2D
+     for(int i=0; i<length; i++){
+      glutBitmapCharacter(GLUT_BITMAP_9_BY_15, (int)text[i]); // generation of characters in our text with 9 by 15 GLU font
+     }
+     glPopMatrix(); // get MODELVIEW matrix value from stack
+     glMatrixMode(GL_PROJECTION); // change current matrix mode to PROJECTION
+     glLoadMatrixd(matrix); // reset
+     glMatrixMode(GL_MODELVIEW); // change current matrix mode to MODELVIEW
 }
 void drawTower(){
     glColor3f(0.0,0.0,1);
@@ -77,14 +78,62 @@ void drawSky(){
         glEnd();
 
 }
+void drawMovingTower(){
+        glColor3f(1.0,0.0,1);
+    	glBegin(GL_QUADS);
+		glVertex3f(corMovingTowerX+5.0, 25.0, corZ);
+		glVertex3f(corMovingTowerX-5.0,25.0, corZ);
+		glVertex3f(corMovingTowerX-5.0,35.0, corZ);
+		glVertex3f(corMovingTowerX+5.0,35.0, corZ);
+        glEnd();
+        glColor3f(0,0,0);
+        glBegin(GL_QUADS);
+        glVertex3f(corMovingTowerX+0.5,35.0, corZ);
+        glVertex3f(corMovingTowerX-0.5,35.0, corZ);
+        glVertex3f(0,100,corZ);
+        glVertex3f(0,100,corZ);
+        glEnd();
+}
+void drawGame(){
+    drawMovingTower();
+    drawTower();
+    drawSky();
+}
 void drawMenu(){
-        glColor3f(1,1,1);
+                if(activeButton==1){
+                    glColor3f(1,0,1);
+                    glBegin(GL_QUADS);
+                    glVertex3f(10.0, 29.0, corZ);
+                    glVertex3f(10.0,25.0, corZ);
+                    glVertex3f(-10.0,25.0, corZ);
+                    glVertex3f(-10.0,29.0, corZ);
+                    glEnd();
+                }else if(activeButton==2){
+                    glColor3f(1,0,1);
+                    glBegin(GL_QUADS);
+                    glVertex3f(10.0, 25.0, corZ);
+                    glVertex3f(10.0,21.0, corZ);
+                    glVertex3f(-10.0,21.0, corZ);
+                    glVertex3f(-10.0,25.0, corZ);
+                    glEnd();
+                }else if(activeButton==3){
+                    glColor3f(1,0,1);
+                    glBegin(GL_QUADS);
+                    glVertex3f(10.0, 21.0, corZ);
+                    glVertex3f(10.0,17.0, corZ);
+                    glVertex3f(-10.0,17.0, corZ);
+                    glVertex3f(-10.0,21.0, corZ);
+                    glEnd();
+                }
+
+        glColor3f(0,1,1);
     	glBegin(GL_QUADS);
 		glVertex3f(100.0, 0.0, corZ);
 		glVertex3f(100.0,100.0, corZ);
 		glVertex3f(-100.0,100.0, corZ);
 		glVertex3f(-100.0,0.0, corZ);
         glEnd();
+
 
         std::string textStartGame;
         textStartGame = "Start Game";
@@ -102,39 +151,19 @@ void drawMenu(){
         drawText(textAboutUs.data(),textAboutUs.size(),365,260);
 
 }
-void drawMovingTower(){
-        glColor3f(1.0,0.0,1);
-    	glBegin(GL_QUADS);
-		glVertex3f(corMovingTowerX+5.0, 25.0, corZ);
-		glVertex3f(corMovingTowerX-5.0,25.0, corZ);
-		glVertex3f(corMovingTowerX-5.0,35.0, corZ);
-		glVertex3f(corMovingTowerX+5.0,35.0, corZ);
-        glEnd();
-        glColor3f(0,0,0);
-        glBegin(GL_QUADS);
-        glVertex3f(corMovingTowerX+0.5,35.0, corZ);
-        glVertex3f(corMovingTowerX-0.5,35.0, corZ);
-        glVertex3f(0,100,corZ);
-        glVertex3f(0,100,corZ);
-        glEnd();
-}
-
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     glTranslatef(0,windowY,0);
-    if(startGame){
-        drawMovingTower();
-        drawTower();
-        drawSky();
-    }else{
-        drawMenu();
 
+    switch(activeScreen){
+        case 1:
+            drawMenu();
+        case 2:
+            drawGame();
     }
-
 }
-
 
 
 int main(int argc, char* args[])
@@ -157,27 +186,43 @@ int main(int argc, char* args[])
                 break;
 
                 case SDL_KEYDOWN:
-                    if(myevent.key.keysym.sym==SDLK_ESCAPE){
-                        (startGame)?startGame=false:startGame=true;
-                        corMovingTowerX=0;
-                        corMovingTowerSpeed=0.02;
-                        corMovingTowerBorder1=15;
-                         corMovingTowerBorder2=-15;
-                        //Translate Screen
-                        windowStart=false;
-                         windowY=-21;
-                         windowSpeed=0.05;
-                       timeWindow=0;
+                    switch(activeScreen){
+                        case 1:
+                            if(myevent.key.keysym.sym==SDLK_UP){
+                                if(activeButton!=1){
+                                    activeButton--;
+                                }
+                            }
+                            if(myevent.key.keysym.sym==SDLK_DOWN){
+                                if(activeButton!=3){
+                                    activeButton++;
+                                }
+                            }
+
+                        case 2:
+                            if(myevent.key.keysym.sym==SDLK_ESCAPE){
+                                corMovingTowerX=0;
+                                corMovingTowerSpeed=0.02;
+                                corMovingTowerBorder1=15;
+                                corMovingTowerBorder2=-15;
+                                //Translate Screen
+                                windowStart=false;
+                                windowY=-21;
+                                windowSpeed=0.05;
+                                timeWindow=0;
+                            }
+                            if(myevent.key.keysym.sym==SDLK_SPACE){
+                                windowStart?windowStart=false:windowStart=true;
+                                timeWindow=0;
+                            }
+
                     }
-                    if(myevent.key.keysym.sym==SDLK_SPACE){
-                        windowStart?windowStart=false:windowStart=true;
-                        timeWindow=0;
-                    }
+
                 break;
             }
         }
 
-        if(startGame){
+        if(activeScreen==2){
             int hit=-1;
             if(1){
                 corMovingTowerX=corMovingTowerSpeed+corMovingTowerX;
