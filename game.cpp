2bc4416bce4896e7ff08331bce97e8ebac1fff05
sezer esim <sezeresim@gmail.com>
2020-01-24 18:54:05 +0100

    color and finish screen function began

--- game.cpp
+++ game.cpp
@@ -8,11 +8,13 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <string>
+#include <sstream>
+using namespace std;
 
 #define M_PI 3.14159265358979323846
 
 
-float corZ=-50;
+float corZ=-60;
 
 //for moving
 float corMovingTowerX=0;
@@ -33,6 +35,13 @@ int activeButton=1;
 //towers
 int endTower=0;
 int towerArray[][2]={{0,5}};
+//for finish screen
+bool finishScreen=false;
+float totalScore=0;
+float calculateScore(float score){
+    totalScore=totalScore+score;
+    return totalScore/endTower+1;
+}
 //initials
 void init(){
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -54,18 +63,32 @@ void drawText(const char *text, int length, int x, int y){
      glLoadIdentity(); // reset it again. (may not be required, but it my convention
      glRasterPos2i(x, y); // raster position in 2D
      for(int i=0; i<length; i++){
-      glutBitmapCharacter(GLUT_BITMAP_9_BY_15, (int)text[i]); // generation of characters in our text with 9 by 15 GLU font
+      glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, (int)text[i]); // generation of characters in our text with 9 by 15 GLU font
      }
      glPopMatrix(); // get MODELVIEW matrix value from stack
      glMatrixMode(GL_PROJECTION); // change current matrix mode to PROJECTION
      glLoadMatrixd(matrix); // reset
      glMatrixMode(GL_MODELVIEW); // change current matrix mode to MODELVIEW
 }
-void drawTower(int positionOfX,int positionOfY){
-     glColor3f(0.4,0.26,0.13);
+void drawFloor(){
+
     glBegin(GL_QUADS);
+        glColor3f(0,0.4,0);
+		glVertex3f(100,-20, corZ);
+		glVertex3f(-100,-20, corZ);
+		glColor3f(0.5,0.8,0);
+		glVertex3f(-100,-35, corZ);
+		glVertex3f(100,-35, corZ);
+
+    glEnd();
+}
+void drawTower(int positionOfX,int positionOfY){
+
+    glBegin(GL_QUADS);
+        glColor3f(0.2,0.26,0.13);
 		glVertex3f(positionOfX+4,positionOfY-4.0, corZ);
 		glVertex3f(positionOfX-4,positionOfY-4.0, corZ);
+		glColor3f(1,0.56,0.13);
 		glVertex3f(positionOfX-4,positionOfY+4.0, corZ);
 		glVertex3f(positionOfX+4,positionOfY+4.0, corZ);
         glEnd();
@@ -79,10 +102,11 @@ void drawTower(int positionOfX,int positionOfY){
 
 }
 void drawSky(){
- glColor3f(0.0,1.0,1);
     	glBegin(GL_QUADS);
-		glVertex3f(100.0, -1000.0, corZ);
+    	glColor3f(0.0,1.0,1);
+		glVertex3f(100.0, -100.0, corZ);
 		glVertex3f(100.0,100.0, corZ);
+		glColor3f(0.0,1.0,0.5);
 		glVertex3f(-100.0,100.0, corZ);
 		glVertex3f(-100.0,-100.0, corZ);
         glEnd();
@@ -96,42 +120,46 @@ void drawHook(){
         glVertex3f(10,100,corZ);
         glVertex3f(-10,100,corZ);
         glEnd();
+
         glColor3f(0,0,0);
         glBegin(GL_QUADS);
-        glVertex3f(corMovingTowerX,38.0, corZ);
-        glVertex3f(corMovingTowerX-5.1,35.0, corZ);
-        glVertex3f(corMovingTowerX-5.1,30,corZ);
-        glVertex3f(corMovingTowerX-7,35,corZ);
+        glVertex3f(corMovingTowerX+7.3,36,corZ);
+        glVertex3f(corMovingTowerX+5.1,30,corZ);
+        glVertex3f(corMovingTowerX+5.1,35,corZ);
+        glVertex3f(corMovingTowerX,38, corZ);
         glEnd();
         glColor3f(0,0,0);
         glBegin(GL_QUADS);
-        glVertex3f(corMovingTowerX,38.0, corZ);
-        glVertex3f(corMovingTowerX+5.1,35.0, corZ);
-        glVertex3f(corMovingTowerX+5.1,30,corZ);
-        glVertex3f(corMovingTowerX+7,35,corZ);
+        glVertex3f(corMovingTowerX-7.3,36,corZ);
+        glVertex3f(corMovingTowerX-5.1,30,corZ);
+        glVertex3f(corMovingTowerX-5.1,35,corZ);
+        glVertex3f(corMovingTowerX,38, corZ);
         glEnd();
         if(!pushState || firstPush){
-              glColor3f(0.4,0.26,0.13);
             glBegin(GL_QUADS);
+            glColor3f(0.2,0.26,0.13);
             glVertex3f(corMovingTowerX+4.0,26.0, corZ);
             glVertex3f(corMovingTowerX-4.0,26.0, corZ);
+            glColor3f(1,0.56,0.13);
             glVertex3f(corMovingTowerX-4.0,34.0, corZ);
             glVertex3f(corMovingTowerX+4.0,34.0, corZ);
             glEnd();
-    glColor3f(0.17,0.1,0.5);
             glBegin(GL_QUADS);
+            glColor3f(0.17,0.1,0.5);
             glVertex3f(corMovingTowerX+5.0,25.0, corZ);
             glVertex3f(corMovingTowerX-5.0,25.0, corZ);
+            glColor3f(0.17,0.1,1);
             glVertex3f(corMovingTowerX-5.0,35.0, corZ);
             glVertex3f(corMovingTowerX+5.0,35.0, corZ);
             glEnd();}
     }
 void drawMovingTower(){
     if(pushState || firstPush){
-              glColor3f(0.4,0.26,0.13);
     	glBegin(GL_QUADS);
+    	glColor3f(0.2,0.26,0.13);
 		glVertex3f(corPushTowerX+4.0,-timeWindow+26.0, corZ);
 		glVertex3f(corPushTowerX-4.0,-timeWindow+26.0, corZ);
+		glColor3f(1,0.56,0.13);
 		glVertex3f(corPushTowerX-4.0,-timeWindow+34.0, corZ);
 		glVertex3f(corPushTowerX+4.0,-timeWindow+34.0, corZ);
         glEnd();
@@ -160,6 +188,23 @@ void drawBestScore(){
             drawText(bestScore.data(),bestScore.size(),358,320);
         }
 }
+void drawScore(){
+    if(activeScreen==2){
+                glBegin(GL_QUADS);
+                    glVertex3f(-25, 50, corZ);
+                    glColor3f(0.8,1,1);
+                    glVertex3f(-25,40, corZ);
+                    glColor3f(0,1,1);
+                    glVertex3f(-40.0,40, corZ);
+                    glColor3f(0.8,0,1);
+                    glVertex3f(-40.0,50, corZ);
+                glEnd();
+                std::string textBestScores;
+                textBestScores = std::to_string(endTower);
+                glColor3f(0, 0, 0);
+                drawText(textBestScores.data(),textBestScores.size(),40,550);
+    }
+}
 void drawGame(){
     drawHook();
     drawMovingTower();
@@ -167,48 +212,65 @@ void drawGame(){
             drawTower(towerArray[i][0],towerArray[i][1]);
 
     }
+    drawScore();
+    if(endTower<2 && activeScreen==2){
+        drawFloor();
+    }
     drawSky();
+
 }
 void drawMenu(){
                 if(activeButton==1){
-                    glColor3f(1,0,1);
                     glBegin(GL_QUADS);
-                    glVertex3f(10.0, 29.0, corZ);
-                    glVertex3f(10.0,25.0, corZ);
-                    glVertex3f(-10.0,25.0, corZ);
-                    glVertex3f(-10.0,29.0, corZ);
+                    glVertex3f(10.0, 30.5, corZ);
+                    glColor3f(0.7,0,1);
+                    glVertex3f(10.0,26.5, corZ);
+                    glVertex3f(-10.0,26.5, corZ);
+                    glColor3f(1,1,1);
+                    glVertex3f(-10.0,30.5, corZ);
                     glEnd();
                 }else if(activeButton==2){
-                    glColor3f(1,0,1);
                     glBegin(GL_QUADS);
-                    glVertex3f(10.0, 25.0, corZ);
-                    glVertex3f(10.0,21.0, corZ);
-                    glVertex3f(-10.0,21.0, corZ);
-                    glVertex3f(-10.0,25.0, corZ);
+                    glVertex3f(10.0, 25.5, corZ);
+                    glColor3f(1,1,1);
+                    glVertex3f(10.0,21.5, corZ);
+                    glVertex3f(-10.0,21.5, corZ);
+                    glColor3f(0.7,0,1);
+                    glVertex3f(-10.0,25.5, corZ);
                     glEnd();
                 }
 
-        glColor3f(0,1,1);
-    	glBegin(GL_QUADS);
-		glVertex3f(100.0, 0.0, corZ);
+       glBegin(GL_QUADS);
+    	glColor3f(0.0,1.0,1);
+		glVertex3f(100.0, -100.0, corZ);
 		glVertex3f(100.0,100.0, corZ);
+		glColor3f(0.0,1.0,0.5);
 		glVertex3f(-100.0,100.0, corZ);
-		glVertex3f(-100.0,0.0, corZ);
+		glVertex3f(-100.0,-100.0, corZ);
         glEnd();
 
 
         std::string textStartGame;
         textStartGame = "Start Game";
         glColor3f(0, 0, 0);
-        drawText(textStartGame.data(), textStartGame.size(), 360, 380);
+        drawText(textStartGame.data(), textStartGame.size(), 340, 380);
 
         std::string textBestScores;
         textBestScores = "Best Score";
         glColor3f(0, 0, 0);
-        drawText(textBestScores.data(),textBestScores.size(),358,320);
+        drawText(textBestScores.data(),textBestScores.size(),338,320);
 
 }
-
+void drawScoreScreen(){
+    glBegin(GL_QUADS);
+    	glColor3f(1.0,1.0,1);
+		glVertex3f(100.0, -100.0, corZ);
+		glVertex3f(100.0,100.0, corZ);
+		glColor3f(1.0,1.0,0.5);
+		glVertex3f(-100.0,100.0, corZ);
+		glVertex3f(-100.0,-100.0, corZ);
+        glEnd();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -222,6 +284,8 @@ void display()
             drawGame();
         case 3:
             drawBestScore();
+        case 4:
+            drawScoreScreen();
     }
 }
 
